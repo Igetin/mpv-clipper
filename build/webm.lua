9ae463751afd6dddc387ffe938cc48e606bd98b6
Igetin <Igetin@users.noreply.github.com>
2020-07-11 02:36:44 +0300

    Update build

--- build/webm.lua
+++ build/webm.lua
@@ -25,38 +25,38 @@ local options = {
 	-- %R - "-(height)p", where height is the video's height, or scale_height, if it's enabled.
 	output_template = "%F-[%s-%e]",
 	-- Scale video to a certain height, keeping the aspect ratio. -1 disables it.
-	scale_height = -1,
+	-- scale_height = -1,
 	-- Target filesize, in kB. This will be used to calculate the bitrate
 	-- used on the encode. If this is set to <= 0, the video bitrate will be set
 	-- to 0, which might enable constant quality modes, depending on the
 	-- video codec that's used (VP8 and VP9, for example).
-	target_filesize = 2500,
+	-- target_filesize = 2500,
 	-- If true, will use stricter flags to ensure the resulting file doesn't
 	-- overshoot the target filesize. Not recommended, as constrained quality
 	-- mode should work well, unless you're really having trouble hitting
 	-- the target size.
-	strict_filesize_constraint = false,
-	strict_bitrate_multiplier = 0.95,
+	-- strict_filesize_constraint = false,
+	-- strict_bitrate_multiplier = 0.95,
 	-- In kilobits.
-	strict_audio_bitrate = 64,
+	-- strict_audio_bitrate = 64,
 	-- Sets the output format, from a few predefined ones.
 	-- Currently we have webm-vp8 (libvpx/libvorbis), webm-vp9 (libvpx-vp9/libvorbis)
 	-- and raw (rawvideo/pcm_s16le).
-	output_format = "webm-vp8",
-	twopass = false,
+	-- output_format = "webm-vp8",
+	-- twopass = false,
 	-- If set, applies the video filters currently used on the playback to the encode.
-	apply_current_filters = true,
+	-- apply_current_filters = true,
 	-- If set, writes the video's filename to the "Title" field on the metadata.
 	write_filename_on_metadata = false,
 	-- Set the number of encoding threads, for codecs libvpx and libvpx-vp9
-	libvpx_threads = 4,
-	additional_flags = "",
+	-- libvpx_threads = 4,
+	-- additional_flags = "",
 	-- Constant Rate Factor (CRF). The value meaning and limits may change,
 	-- from codec to codec. Set to -1 to disable.
 	crf = 15,
 	-- Useful for flags that may impact output filesize, such as qmin, qmax etc
 	-- Won't be applied when strict_filesize_constraint is on.
-	non_strict_additional_flags = "",
+	-- non_strict_additional_flags = "",
 	-- Display the encode progress, in %. Requires run_detached to be disabled.
 	-- On Windows, it shows a cmd popup. "auto" will display progress on non-Windows platforms.
 	display_progress = "auto",
@@ -272,6 +272,29 @@ end
 local starts_with
 starts_with = function(str, start)
   return string.sub(str, 1, #start) == start
+end
+local encoding_profiles
+do
+  local _accum_0 = { }
+  local _len_0 = 1
+  local _list_0 = (mp.get_property_native('profile-list'))
+  for _index_0 = 1, #_list_0 do
+    local p = _list_0[_index_0]
+    if starts_with(p['name'], 'enc-') then
+      _accum_0[_len_0] = p
+      _len_0 = _len_0 + 1
+    end
+  end
+  encoding_profiles = _accum_0
+end
+local get_profile_desc
+get_profile_desc = function(name)
+  for _index_0 = 1, #encoding_profiles do
+    local p = encoding_profiles[_index_0]
+    if name == p['name'] then
+      return p['profile-desc']
+    end
+  end
 end
 local dimensions_changed = true
 local _video_dimensions = { }
@@ -859,20 +882,6 @@ apply_current_filters = function(filters)
     end
   end
 end
-local encoding_profiles
-do
-  local _accum_0 = { }
-  local _len_0 = 1
-  local _list_0 = (mp.get_property_native('profile-list'))
-  for _index_0 = 1, #_list_0 do
-    local p = _list_0[_index_0]
-    if starts_with(p['name'], 'enc-') then
-      _accum_0[_len_0] = p
-      _len_0 = _len_0 + 1
-    end
-  end
-  encoding_profiles = _accum_0
-end
 local encode
 encode = function(region, startTime, endTime)
   local path = mp.get_property("path")
@@ -911,16 +920,19 @@ encode = function(region, startTime, endTime)
     dir = parse_directory(options.output_directory)
   end
   local extension
-  for i, p in ipairs(encoding_profiles) do
+  for _index_0 = 1, #encoding_profiles do
     local _continue_0 = false
     repeat
+      local p = encoding_profiles[_index_0]
       if p['name'] ~= options.encoding_profile then
         _continue_0 = true
         break
       end
-      for i, o in ipairs(p['options']) do
+      local _list_0 = p['options']
+      for _index_1 = 1, #_list_0 do
         local _continue_1 = false
         repeat
+          local o = _list_0[_index_1]
           if o['key'] ~= 'of' then
             _continue_1 = true
             break
@@ -1587,13 +1599,20 @@ do
       local ass = assdraw.ass_new()
       ass:new_event()
       self:setup_text(ass)
-      ass:append(tostring(bold('WebM maker')) .. "\\N\\N")
-      ass:append(tostring(bold('c:')) .. " crop\\N")
-      ass:append(tostring(bold('1:')) .. " set start time (current is " .. tostring(seconds_to_time_string(self.startTime)) .. ")\\N")
-      ass:append(tostring(bold('2:')) .. " set end time (current is " .. tostring(seconds_to_time_string(self.endTime)) .. ")\\N")
-      ass:append(tostring(bold('o:')) .. " change encode options\\N")
-      ass:append(tostring(bold('p:')) .. " preview\\N")
-      ass:append(tostring(bold('e:')) .. " encode\\N\\N")
+      ass:append(tostring(bold('Profile:')) .. " " .. tostring(get_profile_desc(options.encoding_profile)) .. "\\N")
+      ass:append(tostring(bold('CRF:')) .. " " .. tostring(options.crf) .. "\\N")
+      ass:append(tostring(bold('Start time:')) .. " " .. tostring(seconds_to_time_string(self.startTime)) .. "\\N")
+      ass:append(tostring(bold('End time:')) .. " " .. tostring(seconds_to_time_string(self.endTime)) .. "\\N")
+      if self.region.x > 0 and self.region.y > 0 then
+        ass:append(tostring(bold('Crop:')) .. " " .. tostring(self.region.x) .. "×" .. tostring(self.region.y))
+      end
+      ass:append("\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N\\N")
+      ass:append(tostring(bold('C:')) .. " crop\\N")
+      ass:append(tostring(bold('1:')) .. " set start time\\N")
+      ass:append(tostring(bold('2:')) .. " set end time\\N")
+      ass:append(tostring(bold('O:')) .. " change options\\N")
+      ass:append(tostring(bold('P:')) .. " preview\\N")
+      ass:append(tostring(bold('E:')) .. " encode\\N")
       ass:append(tostring(bold('ESC:')) .. " close\\N")
       return mp.set_osd_ass(window_w, window_h, ass.text)
     end,
